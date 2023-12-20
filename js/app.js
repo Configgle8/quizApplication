21fc6b9ee8dc85d7ae6aa9438eac62dba5dbf82e
Michael Anderson <configgle@gmail.com>
2023-12-20 16:22:56 -0600

    Got the containter to completley remove from DOM Style Changes to logo, and button background Created JSON question format Implemented JSON in script and started question methods/fetch Next: JSON Display and Answer input After: Score, submit, and question list, contact.

--- js/app.js
+++ js/app.js
@@ -1,30 +1,106 @@
-const startButton = document.getElementById("start");
-const allButtons = document.querySelectorAll('.button-56');
-const buttonGrid = document.getElementsByClassName("grid");
-const logoLoop = document.getElementsByClassName(".Logo");
-const logo = Array.from(logoLoop);
-var container = document.getElementsByClassName("Container");
+// Global Listener
+function addGlobalEventListener(type, selector, callback) {
+    document.addEventListener(type, e => {
+        if (e.target.matches(selector)) callback(e);
+    });
+}
 
+const startButton = document.getElementById("start");
+const buttonGrid = document.querySelector('.grid'); // Use querySelector to get a single element
+const logoLoop = document.getElementsByClassName("Logo");
+const logo = Array.from(logoLoop);
 
 addGlobalEventListener("click", "#start", e => {
     startGame();
-})
+});
 
-//Global Listener
-function addGlobalEventListener(type, selector, callback){
-    document.addEventListener(type, e => {
-        if (e.target.matches(selector)) callback(e)
-    })
+// Function to fetch quiz data
+async function fetchQuizData() {
+    try {
+        const response = await fetch('quizData.json');
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error fetching quiz data:', error);
+    }
 }
 
-//Will use start game to remove button collection and logo this is where the error occurs. Buttons go away but not logo.
-function startGame(){
-    [...buttonGrid].forEach(button => button.remove());
-    displayQA();
+// Function to display quiz questions
+async function displayQuiz() {
+    const quizData = await fetchQuizData();
+
+    if (!quizData) {
+        // Handle error loading data
+        return;
+    }
+
+    // Use querySelector to get a single element
+    const container = document.querySelector(".Container");
+
+    quizData.forEach((questionObj, index) => {
+        const questionElement = document.createElement('div');
+        questionElement.classList.add('question');
+        questionElement.innerHTML = `<p>${index + 1}. ${questionObj.question}</p>`;
+        questionObj.options.forEach((option, optionIndex) => {
+            const optionElement = document.createElement('input');
+            optionElement.type = 'radio';
+            optionElement.name = `question${index}`;
+            optionElement.value = option;
+            optionElement.id = `q${index}o${optionIndex}`;
+
+            const labelElement = document.createElement('label');
+            labelElement.htmlFor = `q${index}o${optionIndex}`;
+            labelElement.textContent = option;
+
+            questionElement.appendChild(optionElement);
+            questionElement.appendChild(labelElement);
+        });
+
+        container.appendChild(questionElement);
+    });
+}
+
+// Call the displayQuiz function when the DOM content is fully loaded
+document.addEventListener('DOMContentLoaded', displayQuiz);
+
+// Will use start game to remove button collection and logo this is where the error occurs. Buttons go away but not logo.
+function startGame() {
+    buttonGrid.remove(); // Remove the entire grid container
+    logo.forEach(logoElement => logoElement.remove()); // Remove each logo element
+}
+
+ /*   displayQA();
 }
 
 function displayQA(){
-    var questionField = document.createElement("H1");
+    const container = document.getElementsByClassName("Container");
+    quizData.forEach((questionObj, index) => {
+        const questionElement = document.createElement('div');
+        questionElement.classList.add('question');
+        questionElement.innerHTML = `<p>${index + 1}. ${questionObj.question}</p>`;
+        questionObj.options.forEach((option, optionIndex) => {
+            const optionElement = document.createElement('input');
+            optionElement.type = 'radio';
+            optionElement.name = `question${index}`;
+            optionElement.value = option;
+            optionElement.id = `q${index}o${optionIndex}`;
+            
+            const labelElement = document.createElement('label');
+            labelElement.htmlFor = `q${index}o${optionIndex}`;
+            labelElement.textContent = option;
+            
+            questionElement.appendChild(optionElement);
+            questionElement.appendChild(labelElement);
+        });
+        
+        container.appendChild(questionElement);
+    });
+}
+
+// Call the displayQuiz function when the page loads
+window.onload = displayQA;
+
+   /* var questionField = document.createElement("H1");
     var question = document.createTextNode("Question Here?");
     questionField.appendChild(question);
     document.body.appendChild(questionField);
@@ -38,5 +114,4 @@ function displayQA(){
     var answerField = document.createElement("INPUT");
     answerField.setAttribute("type", "text");
     document.body.appendChild(answerField);
-
-}
+*/
